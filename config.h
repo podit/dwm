acd1b1b30b927741db0fe66dd6372100516951e7
podit <tomody@outlook.com>
2020-10-23 21:28:08 +0100

    added tilda support (similar to scratchpad)

--- config.h
+++ config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  { "Tilda",    NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
