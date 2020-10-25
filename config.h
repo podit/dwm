2b19f45a98707fdccd9ae580592b07c875f878dc
podit <tomody@outlook.com>
2020-10-25 04:13:46 +0000

    fixed font spacing issue (upstream problem with ttf-inconslata package) and unified font size with terminal

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "inconsolata:size=10" };
-static const char dmenufont[]       = "inconsolata:size=10";
+static const char *fonts[]          = { "inconsolata:size=9" };
+static const char dmenufont[]       = "inconsolata:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
