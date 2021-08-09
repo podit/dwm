5420e9247e962ea0304eaad4cd632c112e49ba33
podit <tomody@outlook.com>
2021-08-09 19:15:08 +0100

    added goback binds to config

--- config.h
+++ config.h
@@ -76,6 +76,7 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+  { MODKEY,                       XK_g,      goback,         {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
