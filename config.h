e4bf389d5d73bf537ace9193999569b7623f943d
podit <tomody@outlook.com>
2021-08-16 01:29:12 +0100

    applied urgborder patch

--- config.h
+++ config.h
@@ -12,10 +12,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_highlight[]   = "#b87809";
+static const char col_urgborder[]   = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeInv]  = { col_gray1, col_gray3, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_highlight,  col_highlight  },
+	[SchemeUrg]  = { col_gray4, col_highlight,  col_urgborder  },
 };
 
 /* tagging */
